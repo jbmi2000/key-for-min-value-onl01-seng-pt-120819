be24e0d4f9c2a2ada15cb27cad205938dc6392ef
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-20 21:28:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require 'pry'
 def key_for_min_value(name_hash)
 #name_hash.collect do |key, value|
  #puts name_hash.first[1]
- if name_hash = {}
+ if name_hash == {}
      return nil
    else
      binding.pry
