69bba047508dc47d6866d11b9e868bebd8e14faf
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-20 20:03:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-sum=0
-diff=0
 name_hash.collect do |key, value|
- sum=sum+value
- diff=sum-value
- puts diff
+value.high
 end
 end
